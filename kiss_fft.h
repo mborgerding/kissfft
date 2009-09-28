26f8faa6e8c5f1ef2585d1e1ad1b2753d7732b76
Mark Borgerding <mark@borgerding.net>
2009-09-28 15:55:19 +0000

    changed code from memalign to _mm_malloc

--- kiss_fft.h
+++ kiss_fft.h
@@ -27,7 +27,9 @@ extern "C" {
 #ifdef USE_SIMD
 # include <xmmintrin.h>
 # define kiss_fft_scalar __m128
-#define KISS_FFT_MALLOC(nbytes) memalign(16,nbytes)
+    
+//#define KISS_FFT_MALLOC(nbytes) memalign(16,nbytes)
+#define KISS_FFT_MALLOC(nbytes) _mm_malloc(nbytes,16)
 #else	
 #define KISS_FFT_MALLOC malloc
 #endif	
